d4a358ad0424dfb73e881f96a03182a12fdaefbd
aakul <kryaelena85@gmail.com>
2021-04-09 01:15:38 +0300

    Случайное удаление и модификация групп

--- fixture/group.py
+++ fixture/group.py
@@ -34,18 +34,26 @@ class GroupHelper:
         self.change_field_value("group_header", group.header)
         self.change_field_value("group_footer", group.footer)
 
-    def delete_first_group(self):
+    def delete_group_by_index(self, index):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         #submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
         self.group_cache = None
 
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.delete_group_by_index(0)
+
     def select_first_group(self):
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
+
+    def select_group_by_index(self, index):
+        wd = self.app.wd
+        wd.find_elements_by_name("selected[]")[index].click()
 
     def edit_first_group(self, group):
         wd = self.app.wd
@@ -60,10 +68,10 @@ class GroupHelper:
         self.return_to_groups_page()
         self.group_cache = None
 
-    def modify_first_group(self, new_group_data):
+    def modify_group_by_index(self, index, new_group_data):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         # open modification form
         wd.find_element_by_name("edit").click()
         # fill group form
@@ -72,6 +80,10 @@ class GroupHelper:
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
         self.group_cache = None
+
+
+    def modify_first_group(self):
+        self.modify_group_by_index(0)
 
     def return_to_groups_page(self):
         wd = self.app.wd
