89d87d47908640ca65f605727ca1c392bad266fb
aakul <kryaelena85@gmail.com>
2021-04-17 21:13:12 +0300

    добавлена обратгая проверка

--- fixture/contact.py
+++ fixture/contact.py
@@ -129,9 +129,8 @@ class ContactHelper:
                 first_name = cells[2].text
                 last_name = cells[1].text
                 id = cells[0].find_element_by_tag_name("input").get_attribute("value")
-                all_phones = cells[5].text.splitlines()
-                self.contact_cache.append(Contact(firstname=first_name, lastname=last_name, id=id, home=all_phones[0],
-                                                  mobile=all_phones[1], work=all_phones[2], phone2=all_phones[3]))
+                all_phones = cells[5].text
+                self.contact_cache.append(Contact(firstname=first_name, lastname=last_name, id=id, all_phones_from_home_page=all_phones))
         return list(self.contact_cache)
 
 
--- model/contact.py
+++ model/contact.py
@@ -3,8 +3,8 @@ from sys import maxsize
 
 class Contact:
 
-    def __init__(self, firstname=None, lastname=None, home=None,
-                           mobile=None, work=None, phone2=None, id=None):
+    def __init__(self, firstname=None, lastname=None, id=None, home=None, mobile=None, work=None,
+                            phone2=None, all_phones_from_home_page=None):
         self.firstname = firstname
         self.lastname = lastname
         self.home = home
@@ -12,6 +12,7 @@ class Contact:
         self.work = work
         self.phone2 = phone2
         self.id = id
+        self.all_phones_from_home_page = all_phones_from_home_page
 
     def __repr__(self):
         return "%s:%s:%s" % (self.id, self.firstname, self.lastname)
