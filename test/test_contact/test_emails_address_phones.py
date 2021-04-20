d2e6465ec583c2eb39234e6a66d57e46dd23220a
aakul <kryaelena85@gmail.com>
2021-04-20 22:39:15 +0300

    тесты для адрессов, имен, телефонов и эмейлов объеденены в один

---
+++ test/test_contact/test_emails_address_phones.py
@@ -0,0 +1,58 @@
+import re
+
+
+def test_phones_on_home_page(app):
+    contact_from_edit_page, contact_from_home_page = home_edit_page_data(app)
+    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+
+
+def home_edit_page_data(app):
+    contact_from_home_page = app.contact.get_contact_list()[0]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    return contact_from_edit_page, contact_from_home_page
+
+
+def test_phones_on_contact_view_page(app):
+    contact_from_view_page = app.contact.get_contact_from_view_page(0)
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    assert contact_from_view_page.home == contact_from_edit_page.home
+    assert contact_from_view_page.mobile == contact_from_edit_page.mobile
+    assert contact_from_view_page.work == contact_from_edit_page.work
+    assert contact_from_view_page.phone2 == contact_from_edit_page.phone2
+
+
+
+def test_emails_on_home_page(app):
+    contact_from_edit_page, contact_from_home_page = home_edit_page_data(app)
+    assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
+    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
+    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
+    assert contact_from_home_page.address == contact_from_edit_page.address
+
+
+
+def test_names_address_on_home_page(app):
+    contact_from_edit_page, contact_from_home_page = home_edit_page_data(app)
+    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
+    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
+    assert contact_from_home_page.address == contact_from_edit_page.address
+
+
+def clear(s):
+    return re.sub("[() -]", "", s)
+
+
+def merge_emails_like_on_home_page(contact):
+    return "\n".join(filter(lambda x: x != "",
+                            map(lambda x: clear(x),
+                                filter(lambda x: x is not None,
+                                       [contact.email, contact.email2, contact.email3]))))
+
+
+
+
+def merge_phones_like_on_home_page(contact):
+    return "\n".join(filter(lambda x: x != "",
+                            map(lambda x: clear(x),
+                                filter(lambda x: x is not None,
+                                       [contact.home, contact.mobile, contact.work, contact.phone2]))))
