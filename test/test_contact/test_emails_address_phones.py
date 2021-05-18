61734e433ea8fd14c40ae760d6141c089bcc5897
aakul <kryaelena85@gmail.com>
2021-05-18 23:36:57 +0300

    Исправила тест test_compare_names_address_phones_emails_on_home_page_and_db Тест заработал

--- test/test_contact/test_emails_address_phones.py
+++ test/test_contact/test_emails_address_phones.py
@@ -13,18 +13,19 @@ def home_edit_page_data(app):
     return contact_from_edit_page, contact_from_home_page
 
 
-def test_phones_on_contact_view_page(app):
-    contact_from_view_page = app.contact.get_contact_from_view_page(0)
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_view_page.home == contact_from_edit_page.home
-    assert contact_from_view_page.mobile == contact_from_edit_page.mobile
-    assert contact_from_view_page.work == contact_from_edit_page.work
-    assert contact_from_view_page.phone2 == contact_from_edit_page.phone2
+#def test_phones_on_contact_view_page(app):
+#    contact_from_view_page = app.contact.get_contact_from_view_page(0)
+#    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+#    assert contact_from_view_page.home == contact_from_edit_page.home
+#    assert contact_from_view_page.mobile == contact_from_edit_page.mobile
+#    assert contact_from_view_page.work == contact_from_edit_page.work
+#    assert contact_from_view_page.phone2 == contact_from_edit_page.phone2
 
 
 
 def test_emails_on_home_page(app):
-    contact_from_edit_page, contact_from_home_page = home_edit_page_data(app)
+    contact_from_home_page = app.contact.get_contact_list()[0]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
     assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
     assert contact_from_home_page.firstname == contact_from_edit_page.firstname
     assert contact_from_home_page.lastname == contact_from_edit_page.lastname
@@ -39,7 +40,7 @@ def test_names_address_on_home_page(app):
     assert contact_from_home_page.lastname == contact_from_edit_page.lastname
     assert contact_from_home_page.address == contact_from_edit_page.address
 
-def test_compare_names_address_on_home_page_and_db(app, db):
+def test_compare_names_address_phones_emails_on_home_page_and_db(app, db):
     contact_from_home_page = sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
     contact_from_db = db.get_contact_list()
     i = 0
@@ -47,6 +48,8 @@ def test_compare_names_address_on_home_page_and_db(app, db):
         assert contact.firstname.strip() == contact_from_db[i].firstname.strip()
         assert contact.lastname.strip() == contact_from_db[i].lastname.strip()
         assert contact.address == contact_from_db[i].address
+        assert contact.all_emails_from_home_page.strip() == merge_emails_like_on_home_page(contact_from_db[i]).strip()
+        assert contact.all_phones_from_home_page.strip() == merge_phones_like_on_home_page(contact_from_db[i]).strip()
         i=i+1
 
 #    assert contact_from_home_page.address == contact_from_db.address
