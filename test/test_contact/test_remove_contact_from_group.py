8a086bbaf13ef0e686db15363a93919e57f4bf33
aakul <kryaelena85@gmail.com>
2021-05-28 10:32:24 +0300

    Исправления Задание 22:
    тест удаление контакта из группы

--- test/test_contact/test_remove_contact_from_group.py
+++ test/test_contact/test_remove_contact_from_group.py
@@ -3,53 +3,50 @@ import random
 from model.contact import Contact
 
 
-
-
-def test_remove_contact_from_group(app, db, orm):
-    if len(db.get_contact_list()) == 0 and len(db.get_group_list()) == 0:
-        contact = Contact(firstname="group_cont")
+def check_contact_and_group_present(app, db):
+    # проверяем что существует хотя бы 1 контак, если нет, то создаем
+    if len(db.get_contact_list()) == 0:
+        contact = Contact(firstname="check_contact_and_group_present")
         app.contact.create(contact)
-        group = Group(name="group_cont")
+    else:
+        pass
+    # проверяем что существует хотя бы 1 группа, если нет, то создаем
+    if len(db.get_group_list()) == 0:
+        group = Group(name="check_contact_and_group_present")
         app.group.create(group)
-        group = random.choice(db.get_group_list())
-        contact = random.choice(db.get_contact_list())
-        app.contact.add_contact_to_group(contact.id, group.id)
-    elif len(db.get_contact_list()) == 0 and len(db.get_group_list()) > 0:
-        contact = Contact(firstname="group_cont")
-        app.contact.create(contact)
-        contact = random.choice(db.get_contact_list())
-        group = random.choice(db.get_group_list())
-        app.contact.add_contact_to_group(contact.id, group.id)
-    elif len(db.get_contact_list()) > 0 and len(db.get_group_list()) == 0:
-        group = Group(name="group_cont")
-        app.group.create(group)
-        contact = random.choice(db.get_contact_list())
-        group = random.choice(db.get_group_list())
-        app.contact.add_contact_to_group(contact.id, group.id)
+    else:
+        return True
 
+
+def proper_group_to_remove_contact(app, db, orm):
+    # возвращает группу, из которой можно удалить контакт
     i = 0
     lg = len(orm.get_group_list())
+    group = object
+
     for g in orm.get_group_list():
-        if len(orm.get_contacts_in_group(g)) > 0:
-            #contact_id = orm.get_contacts_in_group(g)[0].id
+
+        if len(orm.get_contacts_in_group(g)) > 0:  # проверяем есть ли такие контакты, которые добавлены в эту группу
             group = g
-            group_id = g.id
-            app.contact.remove_contact_from_group(group_id)
             break
         else:
             i = i + 1
-            if i == lg:
-                group = g
-                group_id = g.id
+            if i == lg:  # если групп с добавленными контактами нет, то берем любую и добавляем любой контакт
+                group = random.choice(db.get_group_list())
                 contact = random.choice(db.get_contact_list())
-                app.contact.add_contact_to_group(contact.id, group_id)
-                app.contact.remove_contact_from_group(group_id)
+                app.contact.add_contact_to_group(contact.id, group.id)
                 break
+
+    return group
+
+
+def test_remove_contact_from_group(app, db, orm):
+    check_contact_and_group_present(app, db)
+    group = proper_group_to_remove_contact(app, db, orm)
+    group_id = group.id
+    app.contact.remove_contact_from_group(group_id)
     assert app.contact.check_contact_not_in_group(group_id) == len(orm.get_contacts_not_in_group(group))
     assert len(orm.get_contacts_not_in_group(group)) > 0
-
-
-
 
 
 
